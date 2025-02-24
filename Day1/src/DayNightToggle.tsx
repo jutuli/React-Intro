dead1c1753ed71a3e9cb716386ecccc304471b34
Julia T <juliaturkali@gmail.com>
2025-02-24 15:00:02 +0100

    Task Level 2_2 Add Color Toggling

--- Day1/src/DayNightToggle.tsx
+++ Day1/src/DayNightToggle.tsx
@@ -6,9 +6,14 @@ export default function DayNightToggle() {
   const nightTime = 'Night';
 
   return (
-    <div>
-      <h1>{isDay ? dayTime : nightTime}</h1>
-      <button onClick={() => setIsDay((prev) => !prev)}>
+    <div className={`${isDay ? 'bg-yellow-200' : 'bg-gray-900'}`}>
+      <h1 className={`${isDay ? 'text-black' : 'text-white'}`}>
+        {isDay ? dayTime : nightTime}
+      </h1>
+      <button
+        onClick={() => {
+          setIsDay((prev) => !prev);
+        }}>
         Change to {!isDay ? dayTime : nightTime}
       </button>
     </div>
