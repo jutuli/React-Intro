df0566ec5df4d8c0e12dc2e70b2a4723276e57b7
Julia T <juliaturkali@gmail.com>
2025-02-27 13:30:58 +0100

    add Header

--- Day4/src/components/Header.tsx
+++ Day4/src/components/Header.tsx
@@ -1,5 +1,9 @@
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="align-center my-10 flex justify-center">
+      <h1 className="text-6xl font-bold">Breaking News</h1>
+    </header>
+  );
 };
 
 export default Header;
