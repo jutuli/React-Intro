d0d09700bbffdce5c970cf04b447da47b1a0dbf4
Julia T <juliaturkali@gmail.com>
2025-03-03 12:40:41 +0100

    Update Header component with new styling and title

--- Day5/src/components/Header.tsx
+++ Day5/src/components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <>
-      <header>Header</header>
+      <header className="flex items-center justify-center py-10">
+        <h1 className="text-5xl font-bold">My Wishlist 🎄🎁</h1>
+      </header>
     </>
   );
 };
