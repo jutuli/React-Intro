d45658556a95d1928fa8ddf8e7df93bda09afc33
Julia T <juliaturkali@gmail.com>
2025-03-03 13:41:18 +0100

    Implement AddWish component with form handling and priority selection

--- Day5/src/components/AddWish.tsx
+++ Day5/src/components/AddWish.tsx
@@ -1,8 +1,69 @@
-const AddWish = () => {
+import { useState } from "react";
+
+interface IWish {
+  wish: string;
+  priority: "low-priority" | "medium-priority" | "high-priority" | "none";
+}
+
+interface IAddWishProps {
+  onAddWish: (wish: IWish) => void;
+}
+
+const AddWish = ({ onAddWish }: IAddWishProps) => {
+  const [wish, setWish] = useState<string>("");
+  const [priority, setPriority] = useState<IWish["priority"]>("none");
+
+  const handleSubmit = () => {
+    if (!wish || priority === "none") return;
+    const newWish: IWish = { wish, priority };
+    onAddWish(newWish);
+    setWish("");
+    setPriority("none");
+  };
+
   return (
-    <section>
-      <h2>Add Wish Area</h2>
-    </section>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+      className="flex flex-col items-center justify-center gap-6"
+    >
+      <h2 className="text-2xl font-bold text-gray-500">
+        Add a Wish to Your Wishlist
+      </h2>
+      <div className="flex items-center justify-center gap-4">
+        <input
+          type="text"
+          name="wish"
+          id="wish-input"
+          placeholder="Enter a Wish..."
+          value={wish}
+          onChange={(e) => setWish(e.target.value)}
+          className="h-10 w-60 rounded-md border border-gray-200 p-2 text-base"
+        />
+        <select
+          name="priority"
+          id="priority-select"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          className="h-10 w-60 rounded-md border border-gray-200 p-2 text-base"
+        >
+          <option value="none" disabled hidden>
+            Select a Priority
+          </option>
+          <option value="low-priorty">Low Priority</option>
+          <option value="medium-priority">Medium Priority</option>
+          <option value="high-priority">High Priority</option>
+        </select>
+      </div>
+      <button
+        type="submit"
+        className="w-124 rounded-md bg-green-600 py-2 font-bold text-white"
+      >
+        Add To Wishlist
+      </button>
+    </form>
   );
 };
 
