cc3a5f1c3b27af9f741a9c4e599756e1f742284c
Julia T <juliaturkali@gmail.com>
2025-03-03 12:44:28 +0100

    Enhance Footer component with new styling and thank you message

--- Day5/src/components/Footer.tsx
+++ Day5/src/components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex items-center justify-center">
+      <p className="w-1/4 rounded-md bg-gray-500 py-4 text-center align-bottom font-bold text-white">
+        Thank you! 🎅
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
