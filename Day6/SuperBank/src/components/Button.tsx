d2b691d1dc083287a37fa58cce394fd3cda4f2d5
Julia T <juliaturkali@gmail.com>
2025-03-04 17:11:28 +0100

    Fix Typo in Button.tsx

--- Day6/SuperBank/src/components/Button.tsx
+++ Day6/SuperBank/src/components/Button.tsx
@@ -9,7 +9,6 @@ const Button = ({ text, handleClick }: IButtonProps) => {
       className="px-4 py-2 font-bold text-white bg-blue-400 cursor-pointer rounded-md"
       onClick={handleClick}>
       {text}
-      ref=
     </button>
   );
 };
